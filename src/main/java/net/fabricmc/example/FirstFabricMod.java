4c855e3532689ccf7221324bc40a02d22e67577b
whimsicalities <whimsicalities.code@gmail.com>
2022-05-02 17:16:59 +0100

    Item adding experiment

--- src/main/java/net/fabricmc/example/FirstFabricMod.java
+++ src/main/java/net/fabricmc/example/FirstFabricMod.java
@@ -1,14 +1,20 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class FirstFabricMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Item MY_FABRIC_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 
 	@Override
 	public void onInitialize() {
@@ -17,5 +23,7 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registry.ITEM, new Identifier("tutorial_item", "my_fabric_item"), MY_FABRIC_ITEM);
 	}
 }
